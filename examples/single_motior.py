0fa0aa3a8cc08e8367806f819760f6ada7c862d2
DBinK <DBinKv1@Gmail.com>
2026-03-14 19:26:57 +0800

    chore(env): 添加Python版本配置文件到.gitignore
    - 添加.python-version文件到忽略列表
    - 为uv虚拟环境工具添加忽略规则
    ```

--- examples/single_motior.py
+++ examples/single_motior.py
@@ -7,17 +7,13 @@ from pydamiao.motor import Motor, MotorControl
 
 # 创建电机对象和串口对象
 motor1 = Motor(DamiaoMotorType.DM4310, 0x06, 0x12)
-motor2 = Motor(DamiaoMotorType.DM4310, 0x05, 0x12)
 serial_device = serial.Serial("COM3", 921600, timeout=0.5)
 motor_control = MotorControl(serial_device)
 motor_control.add_motor(motor1)
-motor_control.add_motor(motor2)
 
 # 读取和修改电机参数示例
 if motor_control.switch_control_mode(motor1, ControlType.POS_VEL):
     print("switch POS_VEL success")
-if motor_control.switch_control_mode(motor2, ControlType.VEL):
-    print("switch VEL success")
 
 print("sub_ver:", motor_control.read_motor_param(motor1, MotorVariable.sub_ver))
 print("Gr:", motor_control.read_motor_param(motor1, MotorVariable.Gr))
@@ -27,18 +23,10 @@ print("PMAX:", motor_control.read_motor_param(motor1, MotorVariable.PMAX))
 print("MST_ID:", motor_control.read_motor_param(motor1, MotorVariable.MST_ID))
 print("VMAX:", motor_control.read_motor_param(motor1, MotorVariable.VMAX))
 print("TMAX:", motor_control.read_motor_param(motor1, MotorVariable.TMAX))
-print("Motor2:")
-print("PMAX:", motor_control.read_motor_param(motor2, MotorVariable.PMAX))
-print("MST_ID:", motor_control.read_motor_param(motor2, MotorVariable.MST_ID))
-print("VMAX:", motor_control.read_motor_param(motor2, MotorVariable.VMAX))
-print("TMAX:", motor_control.read_motor_param(motor2, MotorVariable.TMAX))
 
 # 保存并使能电机
-# motor_control.enable(motor3)  # 如果 motor3 未定义，需注释或定义
 motor_control.save_motor_param(motor1)
-motor_control.save_motor_param(motor2)
 motor_control.enable(motor1)
-motor_control.enable(motor2)
 
 # 控制电机运动示例
 i = 0
@@ -47,14 +35,10 @@ while i < 10000:
     i += 1
     # motor_control.control_pos_force(motor1, 10, 1000, 100)
     # motor_control.control_vel(motor1, q * 5)
-    motor_control.control_pos_vel(motor1, q * 8, 3)
+    motor_control.control_pos_vel(motor1, q * 8, 30)
     # print("Motor1:", "POS:", motor1.get_position(), "VEL:", motor1.get_velocity(), "TORQUE:", motor1.get_torque())
     # motor_control.control_mit(motor2, 35, 0.1, 8 * q, 0, 0)
 
-    motor_control.control_vel(motor2, 8 * q)
-    # print("Motor2:", "POS:", motor2.get_position(), "VEL:", motor2.get_velocity(), "TORQUE:", motor2.get_torque())
-    # print(motor1.get_torque())
-    # print(motor2.get_torque())
     time.sleep(0.001)
     # motor_control.control(motor3, 50, 0.3, q, 0, 0)
 
